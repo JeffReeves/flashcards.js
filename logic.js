5d9fcb2bc59c98c63292f92b483023c1f001d6b7
JeffReeves <jeff@alchemist.digital>
2017-08-09 17:15:25 -0700

    Updated layout and added flip animation

--- logic.js
+++ logic.js
@@ -11,9 +11,30 @@ var deckSelected = 0;
 var numCards = decks[deckSelected].cards.length;
 // generate a random number between 0 and the total number in the deck
 var randomCard = Math.floor(Math.random() * (numCards - 1));
+// debugging
+randomCard = 63;
 
 console.log('random card: ', randomCard);
 
 // place the contents of the random card within the front and back elements
 front.innerText = decks[deckSelected].cards[randomCard].front;
 back.innerText = decks[deckSelected].cards[randomCard].back;
+
+
+// add class to flip the flashcard over when hovered over
+var flashcardContainer = document.getElementById('flashcard-container');
+var flashcard = document.getElementById('flashcard');
+
+// add event for mouse enter
+flashcardContainer.addEventListener("mouseover", function(){
+
+    // add class to flip card
+    flashcard.className = 'flipped';
+});
+
+// add event for mouse exit
+flashcardContainer.addEventListener("mouseout", function(){
+
+    // remove class to unflip
+    flashcard.removeAttribute('class');
+});
