cc5470174f3ba33476e9ff8e4b33dd5f53cc2cb0
JeffReeves <jeff@alchemist.digital>
2017-08-14 08:36:58 +0000

    First query

--- api/api.js
+++ api/api.js
@@ -3,27 +3,48 @@ var router = express.Router();
 
 var db = require('../database.js');
 
-db.getConnection(function(err, connection){
-
-  // Use the connection
-  connection.query('SELECT * FROM cards', function (error, results, fields) {
-
-    if(results){
-        console.log('[DEBUG] results: ', results);
-    }
-    
-    // And done with the connection.
-    connection.release();
-
-    // Handle error after the release.
-    if (error) throw error;
-  });
-});
-
 //== ROUTES ===================================================================
 
 router.get('/test', function(req, res){
 
+    db.getConnection(function(err, connection){
+
+        // Use the connection
+        connection.query('SELECT * FROM cards', function (error, results, fields) {
+
+            if(results){
+                console.log('[DEBUG] api test results: ', results);
+            }
+                
+            // And done with the connection.
+            connection.release();
+
+            // Handle error after the release.
+            if (error) throw error;
+        });
+    });
+});
+
+router.get('/cards/all', function(req, res) {
+
+    // select all cards
+    var query = 'SELECT * FROM cards';
+
+    db.getConnection(function(err, connection){
+
+        connection.query(query, function (error, results, fields) {
+
+            if(results){
+                res.send(results);
+            }
+                
+            connection.release();
+
+            if (error){
+                res.send(error);
+            }
+        });
+    });
 });
 
 router.get('/decks/all', function(req, res){
