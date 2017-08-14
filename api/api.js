40fe92ebc32c0b3934908eba9450741f0e0b12e0
JeffReeves <jeff@alchemist.digital>
2017-08-14 20:08:49 +0000

    Added deck selection

--- api/api.js
+++ api/api.js
@@ -5,6 +5,36 @@ var db = require('../database.js');
 
 //== ROUTES ===================================================================
 
+router.get('/decks/:username', function(req, res) {
+
+    // select all decks for the username
+    var username = req.params.username;
+
+    var query = 'SELECT decks.id, decks.title ' + 
+                'FROM decks INNER JOIN users ON decks.userid=users.id ' +
+                'WHERE users.username = "' + username + '";';    
+
+    db.getConnection(function(err, connection){
+
+        connection.query(query, function(error, results, fields) {
+
+            connection.release();
+
+            //var userid = JSON.parse(results[0]);
+
+            //var json = JSON.stringify(userid);
+
+            if(results){
+                res.send(results);
+            }
+                
+            if(error){
+                res.send(error);
+            }
+        });
+    });
+});
+
 router.get('/cards/all', function(req, res) {
 
     // select all cards
--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-  connectionLimit : 10,
   host            : 'localhost',
   user            : 'flash',
   password        : 'Electronic#Supersonic!',
-  database        : 'flashcardsjs'
+  database        : 'flashcardsjs',
+  connectionLimit : 10,
+  multipleStatements: true
 });
 
 module.exports = pool;
