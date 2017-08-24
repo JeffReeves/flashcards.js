af89549d80b04f337bf6aad6395a1f06b3ebf0c1
JeffReeves <jeff@alchemist.digital>
2017-08-24 00:40:46 -0700

    Added basic card flip

--- flashcards.js/src/app/cards/cards.component.ts
+++ flashcards.js/src/app/cards/cards.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cards',
-  templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+    selector: 'app-cards',
+    templateUrl: './cards.component.html',
+    styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
 
-  selectedValue: string;
+    flipped = false;
+    
+    selectedValue: string;
 
-  decks = [
-    { id: 0, title: 'CH1 Command Line' },
-    { id: 1, title: 'CH2 Package Managers' },
-    { id: 2, title: 'CH2 Libraries' },
-    { id: 3, title: 'CH2 Processes' }
-  ];
+    decks = [
+        { id: 0, title: 'CH1 Command Line' },
+        { id: 1, title: 'CH2 Package Managers' },
+        { id: 2, title: 'CH2 Libraries' },
+        { id: 3, title: 'CH2 Processes' }
+    ];
 
-  constructor() { 
-  }
+    constructor() { 
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    flipCard(flipDirection: string): void {
+
+        if(flipDirection === 'back') {
+            this.flipped = true;
+        }
+        else {
+            this.flipped = false;
+        }
+    }
 
 }
