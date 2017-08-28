bbb26e007f9dd89bb1ec79468114b55915c25bf8
JeffReeves <jeff@alchemist.digital>
2017-08-27 17:33:45 -0700

    Started deck dropdown logic

--- client/scripts/client-controller.js
+++ client/scripts/client-controller.js
@@ -1,4 +1,5 @@
-// dependencies: jQuery v3.2.1, functions.js
+// 3rd party dependencies: jQuery v3.2.1, mui-0.9.22
+// local dependencies: functions.js
 
 //const _apiUrl = 'https://alchemist.digital/flashcards/api/'; // PROD API
 const _apiUrl = '../test-api/'; // TEST API
@@ -8,33 +9,32 @@ var User = (function(){
     function User(username){
         this.username = username;
         this.id = 0;
-        this.currentDeck = {};
         this.decks = [];
 
         // get user id from db
-        this.getUserIdJSON(_apiUrl + 'users/' + this.username);
+        this.getUserId(_apiUrl + 'users/' + this.username);
     }
 
-    User.prototype.getUserIdJSON = function(url){
+    User.prototype.getUserId = function(url){
         var that = this;
         $.getJSON(url, function(response) {
             // set user id
             that.id = response[0].id;
 
             // get all decks that belong to that user id
-            that.getDecksJSON(_apiUrl + 'decks/user/' + that.id);
+            that.getDecks(_apiUrl + 'decks/user/' + that.id);
             return that.id;
         });        
     }
 
-    User.prototype.getDecksJSON = function(url){
+    User.prototype.getDecks = function(url){
         var that = this;
         $.getJSON(url, function(response) {
 
-            for(var i = response.length; i--;){
+            for(var i = 0; i < response.length; i++){
                 that.decks.push(new Deck(response[i]));
             }
-            return that.decks.reverse();
+            return that.decks;
         });
     }
 
@@ -42,25 +42,24 @@ var User = (function(){
 }());
 
 var Deck = (function(){
-
-    var _id = 1;
     
     function Deck(obj){
         this.title = obj.title;
-        this.id = obj.id || _id++;
+        this.id = obj.id || null;
+        this.group = obj.group;
         this.cards = [];
 
-        this.getCardsJSON(_apiUrl + 'cards/deckid/' + this.id);
+        this.getCards(_apiUrl + 'cards/deckid/' + this.id);
     }
 
-    Deck.prototype.getCardsJSON = function(url){
+    Deck.prototype.getCards = function(url){
         var that = this;
         $.getJSON(url, function(response) {
 
-            for(var i = response.length; i--;){
+            for(var i = 0; i < response.length; i++){
                 that.cards.push(new Card(response[i]));
             }
-            return that.cards.reverse();
+            return that.cards;
         });
     }
 
@@ -69,16 +68,188 @@ var Deck = (function(){
 
 var Card = (function(){
 
-    var _id = 1;
-
     function Card(obj){
         this.front = obj.front;
         this.back = obj.back;
-        this.id = obj.id || _id++;
+        this.id = obj.id || null;
     }
 
     return Card;
 }());
 
+var Modal = (function(){
+
+    var _element = document.createElement('div');
+    _element.id = 'loginModal'
+
+    var _options = {
+        'keyboard': true, 
+        'static': false, 
+        'onclose': function() {
+                console.log('modal closed');
+            } 
+        };
+    
+    function Modal(){
+        this.overlay = {};
+    }
+
+    Modal.prototype.open = function(){
+        mui.overlay('on', _options, _element);
+    }
+
+    Modal.prototype.close = function(){
+        mui.overlay('off');
+    }
+    
+    return Modal;
+}());
+
+var Interface = (function(){
+    
+    function Interface(){
+        this.elements = {
+            // controls 
+            dropdownMenu: document.getElementById('dropdown-menu'),
+    
+            // menu options
+            menuCardView: document.getElementById('menu-card-view'),
+            menuEditView: document.getElementById('menu-edit-view'),
+            menuLoginModal: document.getElementById('menu-login-modal'),
+    
+            // views
+            cardView: document.getElementById('cardView'),
+            editorView: document.getElementById('editorView'),
+    
+            // flashcards
+            flashcardContainer: document.getElementById('flashcard-container'),
+            flashcard: document.getElementById('flashcard'),
+            front: document.getElementById('front'),
+            back: document.getElementById('back'),
+            frontText: front.getElementsByTagName('p')[0],
+            backText: back.getElementsByTagName('p')[0],
+    
+            // progress bar
+            progressBar: document.getElementById('progress-bar'),
+            correctProgress: document.getElementById('correct-progress'),
+            incorrectProgress: document.getElementById('incorrect-progress'),
+            skippedProgress: document.getElementById('skipped-progress'),
+    
+            // dropdown to select decks
+            deckSelect: document.getElementById('deck-select'),
+    
+            // buttons
+            correctButton: document.getElementById('correct'),
+            incorrectButton: document.getElementById('incorrect'),
+            skipButton: document.getElementById('skip')
+        };
+
+        this.current = {
+            username: '',
+            userId: 0,
+            decks: [],
+            deck: {},
+            cards: [],
+            card: {},
+            totalCards: 0,
+            correct: 0,
+            incorrect: 0,
+            skipped: 0
+        };
+    }
+
+    Interface.prototype.userLogin = function(user){
+
+        // user details
+        this.current.username = user.username;
+        this.current.userId = user.id;
+
+        // deck details
+        this.current.decks = user.decks;
+    }
+
+    Interface.prototype.addOptions = function(options){
+    // options = [{name: '', decks: [{id: 0, title: ''}]}]
+
+        for(var i = 0; i < options.length; i++){
+
+            // add option group for deck groups
+            var optionGroup = document.createElement('optgroup');
+            optionGroup.label = options[i].name;
+
+            for(var j = 0; j < options[i].decks.length; j++){
+
+                // add option for each deck
+                var option = document.createElement('option');
+                option.setAttribute('value', options[i].decks[j].id);
+                option.text = options[i].decks[j].title;
+
+                // append it to the select dropdown
+                optionGroup.appendChild(option);
+            }
+
+            // append it to the select dropdown
+            this.elements.deckSelect.appendChild(optionGroup);
+        }       
+    }
+
+    Interface.prototype.setupDeckSelection = function(){
+
+        var self = this;
+        var decks = self.current.decks;  
+        var groupsNames = [];
+        var options = []; 
+
+        // iterate through each deck
+        for(var i = 0; i < decks.length; i++){
+
+            // add the group to the list if it's not there already
+            if(groupsNames.indexOf(decks[i].group) === -1){
+                groupsNames.push(decks[i].group);
+            }
+        }
+
+        // iterate through each group on the group list
+        for(var i = 0; i < groupsNames.length; i++){
+
+            var deckGroups = [];
+
+            // and each deck
+            for(var j = 0; j < decks.length; j++){
+
+                var deck = {
+                    id: 0,
+                    title: ''
+                };
+
+                if(groupsNames[i] === decks[j].group){
+                    deck.id = decks[j].id;
+                    deck.title = decks[j].title;
+
+                    deckGroups.push(deck);
+                }
+            }
+
+            // add the name and titles to the options list
+            options.push({
+                name: groupsNames[i],
+                decks: deckGroups
+            });
+        }
+
+        this.addOptions(options);
+
+
+        // create an onchange event to switch selected deck
+        this.elements.deckSelect.addEventListener('change', function(){
+           console.log(this.value);
+        });
+    }
+
+    return Interface;
+}());
+
 // TEST
+var modal = new Modal();
+var interface = new Interface();
 //var user = new User('jeff'); // will eventually create user based on input into login modal
