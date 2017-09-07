0e6780ac37f7dae7dbad70b6ce89532011eba530
JeffReeves <jeff@alchemist.digital>
2017-09-06 23:11:22 -0700

    added status property to cards

--- client/scripts/classes/card.js
+++ client/scripts/classes/card.js
@@ -1,9 +1,10 @@
 export class Card {
     
-    constructor(deckId, id, front, back){
+    constructor(deckId, id, front, back, status){
         this.deckId = deckId;
         this.id = id;
         this.front = front;
         this.back = back;
+        this.status = status || null;
     }
 }
